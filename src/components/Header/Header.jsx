605b738e381f3172f805b04da8fd3a89dcda61f7
Natalia <mcb.nat@gmail.com>
2020-03-26 03:23:41 +0100

    Update header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import './Header.css';
 
 const Header = () => {
-  return <header></header>;
+  return (
+    <header>
+      <Navbar variant='dark'>
+        <Container fluid>
+          <Navbar.Brand>Koronawirus w woj. podlaskim</Navbar.Brand>
+          <Nav className='nav-right'>
+            <Nav.Link href='https://www.gov.pl/web/koronawirus'>Informacje i zalecenia</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </header>
+  );
 };
 
 export default Header;
