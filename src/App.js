59cd56474900f77289a94400cb51a89aaf00d7e2
Natalia <mcb.nat@gmail.com>
2020-03-26 03:15:19 +0100

    Add cases data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
+import CasesJSON from './przypadki.json';
 import './App.css';
 
 function App() {
+  const [cases, setCases] = useState([]);
+
+  useEffect(() => {
+    setCases(CasesJSON);
+  }, []);
+
   return (
-    <div className="App">
+    <div className='App'>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
