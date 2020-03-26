70717fd4d3e373f1c85546814e5ee4a33a50e70a
Natalia <mcb.nat@gmail.com>
2020-03-26 03:24:32 +0100

    Add displaying cases list

--- src/components/CasesList/CasesList.jsx
+++ src/components/CasesList/CasesList.jsx
@@ -1,10 +1,40 @@
 import React from 'react';
+import Moment from 'moment';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import Badge from 'react-bootstrap/Badge';
 import './CasesList.css';
 
 const CasesList = ({ cases }) => {
-  return <Col lg={4} className='cases-list'></Col>;
+  return (
+    <Col lg={4} className='cases-list'>
+      <div className='counter'>
+        <h3>
+          <Badge pill variant='dark' className='cases-pill'>
+            {cases.suma}
+          </Badge>
+          zakażeń
+        </h3>
+      </div>
+      <div className='cases-update'>
+        <p>Ostatnia aktualizacja: {Moment(cases.aktualizacja).format('DD.MM.YYYY HH:mm')}</p>
+      </div>
+      <Col className='list'>
+        {cases.powiaty &&
+          cases.powiaty.length &&
+          cases.powiaty.map((powiat, index) => (
+            <Row className='list-item' key={powiat.kod}>
+              <Col md={10} className='list-item-title'>
+                {powiat.nazwa}
+              </Col>
+              <Col md={2} className='list-item-value'>
+                {powiat.potwierdzone}
+              </Col>
+            </Row>
+          ))}
+      </Col>
+    </Col>
+  );
 };
 
 export default CasesList;
