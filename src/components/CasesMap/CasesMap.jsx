15c4ed992f56db4e25bec09012f5d1c4870623c3
Natalia <mcb.nat@gmail.com>
2020-03-26 10:52:50 +0100

    Add basic leaflet map

--- src/components/CasesMap/CasesMap.jsx
+++ src/components/CasesMap/CasesMap.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import Col from 'react-bootstrap/Col';
+import { Map as LeafletMap, TileLayer } from 'react-leaflet';
 import './CasesMap.css';
 
 const CasesMap = () => {
-  return <Col className='cases-map'></Col>;
+  const position = [53.128, 23.114];
+  const zoom = 8;
+
   return (
     <Col md={8} className='cases-map px-0'>
+      <LeafletMap center={position} zoom={zoom}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
+        />
+      </LeafletMap>
     </Col>
   );
 };
