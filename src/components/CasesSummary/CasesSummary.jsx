76d0a21400df9bc925bc49329c776091c5b18494
Natalia <mcb.nat@gmail.com>
2020-04-08 12:09:46 +0200

    Add tooltip for daily growth indicator

--- src/components/CasesSummary/CasesSummary.jsx
+++ src/components/CasesSummary/CasesSummary.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-// import { ArrowUp } from 'react-bootstrap-icons';
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
+import { IoMdHelp } from 'react-icons/io';
 import { useData } from '../../contexts/DataContext';
 import { Counter } from '../index';
 import './CasesSummary.css';
@@ -8,20 +9,43 @@ import GrowthIndicator from '../GrowthIndicator/GrowthIndicator';
 
 const CasesSummary = () => {
   const { cases, deaths, cures } = useData();
+  const showTooltip = props => <Tooltip {...props}>Dzisiejsza zmiana</Tooltip>;
 
   return (
-    <div className='cases-summary'>
+    <div
+      className='cases-summary'
+      style={cases.today || deaths.today || cures.today ? { width: 218 } : { width: 165 }}
+    >
       <div className='active-cases'>
         <Counter data={cases.total} label={'zakażeń'} color={Colors.primaryLight} />
-        {cases.today > 0 && <GrowthIndicator count={cases.today} />}
+        {cases.today > 0 && (
+          <OverlayTrigger overlay={showTooltip} trigger={'click'}>
+            <div className='daily-growth-wrapper'>
+              <GrowthIndicator count={cases.today} />
+              <IoMdHelp className='info' />
+            </div>
+          </OverlayTrigger>
+        )}
       </div>
       <div className='deaths'>
         <Counter data={deaths.total} label={'zgonów'} color={Colors.negative} />
-        {deaths.today > 0 && <GrowthIndicator count={deaths.today} />}
+        {deaths.today > 0 && (
+          <OverlayTrigger overlay={showTooltip}>
+            <div className='daily-growth-wrapper'>
+              <GrowthIndicator count={deaths.today} />
+            </div>
+          </OverlayTrigger>
+        )}
       </div>
       <div className='cures'>
         <Counter data={cures.total} label={'wyleczeń'} color={Colors.positive} />
-        {cures.today > 0 && <GrowthIndicator count={cures.today} />}
+        {cures.today > 0 && (
+          <OverlayTrigger overlay={showTooltip}>
+            <div className='daily-growth-wrapper'>
+              <GrowthIndicator count={cures.today} />
+            </div>
+          </OverlayTrigger>
+        )}
       </div>
     </div>
   );
